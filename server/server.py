e8b765cd1535150ab8aeda44e3164b04e6af480c
Ebad Ansari <ebadansari414@gmail.com>
2025-04-18 23:41:26 +0530

    Full Stack app.

--- server/server.py
+++ server/server.py
@@ -1,12 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import util
 
-app = Flask(__name__)
-
-
-@app.route('/', methods=['GET'])
-def helloworld(): 
-    return jsonify({"data": "Hello World"})
+app = Flask(__name__, static_folder="static")
 
 @app.route('/api/get_location_names', methods=['GET'])
 def get_location_names():
@@ -36,4 +31,4 @@ def predict_home_price():
 # if __name__ == "__main__":
 print("Starting Python Flask Server For Home Price Prediction...")
 util.load_saved_artifacts()
-app.run(host="0.0.0.0", port=5000)
+app.run()
